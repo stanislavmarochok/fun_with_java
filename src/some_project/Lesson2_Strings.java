1e2b58fd44a2346c6e6407354508f178be370ec9
Stas Marochok <ssssstas30@gmail.com>
2018-09-05 09:13:14 +0300

    concate(), replace(), toUpperCase(), trim()

--- src/some_project/Lesson2_Strings.java
+++ src/some_project/Lesson2_Strings.java
@@ -2,11 +2,23 @@ package some_project;
 
 public class Lesson2_Strings {
 
-	public static void main(String[] args) {
+	public static void Lesson2(String[] args) {
 		String s = "азадеревомдеревоазадеревомлес";
 		
 		System.out.println(s.indexOf("д"));
 		System.out.println(s.indexOf("дер"));
+		System.out.println(s.indexOf("дер", 14));
+		
+		String a = "Енот ";
+		String b = "полоскун";
+		System.out.println(a + b);
+		System.out.println(a.concat(b));
+		System.out.println(a.replace("т", "Д"));
+		System.out.println(b.toUpperCase());
+		
+		String c = "     крендель     ";
+		System.out.println(c);
+		System.out.println(c.trim());
 	}
 
 }
