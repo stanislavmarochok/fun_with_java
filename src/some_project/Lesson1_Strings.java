676584f936ec858069653411440041de541a6d0f
Stas Marochok <ssssstas30@gmail.com>
2018-09-05 08:11:36 +0300

    Learned what is for (String s: arr) and how to use it

---
+++ src/some_project/Lesson1_Strings.java
@@ -0,0 +1,13 @@
+package some_project;
+
+public class Lesson1_Strings {
+
+	public static void main(String[] args) {
+		String[] food = {"фенхель", "пармизан", "лапша", "банан", "патока"};
+		for (String s: food)
+		{
+			System.out.println(s);
+		}
+	}
+
+}
